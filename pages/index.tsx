e8af76b8fe4a71b25237b0d897a862a7ca8d72fc
TugiatheNarrator <tugia.narrator@gmail.com>
2025-12-13 15:16:19 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,9 +53,6 @@ const AuthenticationPage = () => {
   const { play } = useSoundPlayer()
   const { isContentReady } = useMainStore()
 
-  const title = `"The Incredibles" - Syndrome Main Computer`
-  const description = `A faithful, modern-day web recreation of Syndrome’s iconic main computer (Brainrot unveiled sequence), built with Next.js and React 19. This project brings to life the cinematic interface from The Incredibles with cutting-edge web technologies.`
-
   const SUBMIT_TIMEOUT = 300
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
