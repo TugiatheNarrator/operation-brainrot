430832dbd75cff4d8e0284ff80ae92284f1a970b
TugiatheNarrator <tugia.narrator@gmail.com>
2025-12-13 18:57:14 -0800

    Update encounters.ts

--- data/encounters.ts
+++ data/encounters.ts
@@ -1,7 +1,7 @@
 import { Encounter } from '@/types'
 
 export const encounters: Array<Encounter> = [
-  { superSlug: 'millennials, omnidroidSlug: 'rickrolling', superDefeated: true, omnidroidDefeated: false },
+  { superSlug: 'millennials', omnidroidSlug: 'rickrolling', superDefeated: true, omnidroidDefeated: false },
   { superSlug: 'generation-z', omnidroidSlug: 'rickrolling', superDefeated: false, omnidroidDefeated: true },
   { superSlug: 'generation-z', omnidroidSlug: 'mlg-brainrot', superDefeated: true, omnidroidDefeated: false },
   { superSlug: 'generation-alpha', omnidroidSlug: 'mlg-brainrot', superDefeated: false, omnidroidDefeated: true },
