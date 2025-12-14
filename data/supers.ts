c90a65d08b2b2c521f3af98484ae9bc2dd4fed5b
TugiatheNarrator <tugia.narrator@gmail.com>
2025-12-13 17:54:43 -0800

    Update supers.ts

--- data/supers.ts
+++ data/supers.ts
@@ -1,9 +1,9 @@
 import { Super, SuperResult } from '@/types'
 
 export const supers: Array<Super> = [
-  { name: 'Millennials', slug: 'millennials', threatRating: 2.9, description: 'Atomic density manipulation', img: 'universal_man.png' },
-  { name: 'Generation Z', slug: 'generation-z', threatRating: 1.6, description: 'Psychic foresight, thought control, telekinesis', img: 'psycwave.png' },
-  { name: 'Generation Alpha', slug: 'generation-alpha', threatRating: 3.2, description: 'Telepathy, clairvoyance, magni-vision', img: 'everseer.png' },
+  { name: 'Millennials', slug: 'millennials', threatRating: 2.1, description: 'Social media', img: 'universal_man.png' },
+  { name: 'Generation Z', slug: 'generation-z', threatRating: 2.5, description: 'Smartphone', img: 'psycwave.png' },
+  { name: 'Generation Alpha', slug: 'generation-alpha', threatRating: 4.1, description: 'iPad', img: 'everseer.png' },
 ]
 
 export const supersResult: Array<SuperResult> = [
@@ -12,19 +12,39 @@ export const supersResult: Array<SuperResult> = [
     slug: 'generation-x',
     img: 'generation-x.png',
     description:
-      'Full-body elasticity, body capable of stretching in extreme way up to three hundred feet, high durability to physical injuries, stretch assisted leaps span eighty feet. Can compress and expand shape limited to maintaining bodily volume.',
-    lastActiveRecord: '13.11.55',
-    locationKnown: false,
-    threatRating: '6.2 Exercise moderate caution',
+      'Computer',
+    lastActiveRecord: '31.12.80',
+    locationKnown: true,
+    threatRating: '6.1 Exercise moderate caution',
   },
   {
     name: 'Baby boomers',
     slug: 'baby-boomers',
     img: 'baby-boomers.png',
     description:
-      'Spontaneous generation of ice from water sources, freeze surface areas in excess of five hundreds meters. Manipulation of water for the formation of ice in solid form and structures in this material. He can generate ice from his fingertips but is limited by the amount of moisture in the air and his body. ',
-    lastActiveRecord: '17.12.56',
+      'TV ',
+    lastActiveRecord: '31.12.64',
     locationKnown: true,
-    threatRating: '8.0 Exercise moderate caution',
+    threatRating: '6.7 Exercise moderate caution',
+  },
+  {
+    name: 'Silent Generation',
+    slug: 'silent-generation',
+    img: 'silent-generation.png',
+    description:
+      'Impossible to corrupt ',
+    lastActiveRecord: '31.12.45',
+    locationKnown: true,
+    threatRating: '6.9 Exercise moderate caution',
+  },
+  {
+    name: 'Greatest Generation',
+    slug: 'greatest-generation',
+    img: 'greatest-generation.png',
+    description:
+      'Impossible to corrupt ',
+    lastActiveRecord: '31.12.27',
+    locationKnown: true,
+    threatRating: '9.3 Exercise high caution',
   },
 ]
